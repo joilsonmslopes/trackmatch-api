cabef1f81f66d7c5bb244a2d652817ccfa114910
Joilson Miranda <joilsonmirandasl@hotmail.com>
2025-05-27 17:24:03 -0300

    feat: initial commit

---
+++ src/main/java/com/trackmatch/TrackMatchApplication.java
@@ -0,0 +1,13 @@
+package com.trackmatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackMatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackMatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trackmatch/TrackMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trackmatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackMatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
