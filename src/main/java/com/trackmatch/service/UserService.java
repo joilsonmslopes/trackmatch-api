717c02716b2f4921beb2014be88d7139aeda9b5a
Joilson Miranda <joilsonmirandasl@hotmail.com>
2025-06-07 21:55:54 -0300

    feat: add updateUserById method in UserService

--- src/main/java/com/trackmatch/service/UserService.java
+++ src/main/java/com/trackmatch/service/UserService.java
@@ -3,44 +3,49 @@ package com.trackmatch.service;
 import com.trackmatch.domain.entities.UserEntity;
 import com.trackmatch.dto.user.UserDTO;
 import com.trackmatch.dto.user.UserMapper;
+import com.trackmatch.dto.user.UserPatchDTO;
+import com.trackmatch.dto.user.UserPatchMapper;
 import com.trackmatch.exception.EntityType;
 import com.trackmatch.exception.NotFoundException;
 import com.trackmatch.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor        // injeta dependências via construtor
 public class UserService {
-    @Autowired
-    private UserRepository userRepository;
-    @Autowired
-    private UserMapper userMapper;
+
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+    private final UserPatchMapper userPatchMapper;
 
     public List<UserDTO> getAllUsers() {
-        List<UserEntity> userEntities = userRepository.findAll();
-        List<UserDTO> userDTOs = new ArrayList<>();
-
-        for (UserEntity userEntity : userEntities) {
-            userDTOs.add(userMapper.map(userEntity));
-        }
-
-        return userDTOs;
+        List<UserEntity> users = userRepository.findAll();
+        return userMapper.toDTOs(users);
     }
 
     public UserDTO getUserById(long id) {
-        UserEntity userFound = userRepository.findById(id).orElseThrow(() -> new NotFoundException(EntityType.USER, id));
-
-        return userMapper.map(userFound);
+        UserEntity user = userRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException(EntityType.USER, id));
+        return userMapper.toDTO(user);
     }
 
-    public UserDTO createUser(UserDTO user) {
-        UserEntity userEntity = userMapper.map(user);
-        userEntity = userRepository.save(userEntity);
-
-        return userMapper.map(userEntity);
+    public UserDTO createUser(UserDTO dto) {
+        UserEntity entityToSave = userMapper.toEntity(dto);
+        UserEntity saved = userRepository.save(entityToSave);
+        return userMapper.toDTO(saved);
     }
 
+    public UserDTO updateUserById(long id, UserPatchDTO userDto) {
+        UserEntity userEntity = userRepository.findById(id).orElseThrow(() -> new NotFoundException(EntityType.USER, id));
+
+        userPatchMapper.updateUserFromDto(userDto, userEntity);
+
+        userRepository.save(userEntity);
+
+        return userMapper.toDTO(userEntity);
+    }
 }
