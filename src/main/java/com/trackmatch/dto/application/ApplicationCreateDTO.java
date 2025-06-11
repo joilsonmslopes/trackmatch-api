966cc73ee6c54584fc8d058efcdbb3cacc077144
Joilson Miranda <joilsonmirandasl@hotmail.com>
2025-06-10 22:46:21 -0300

    fix: remove unused imports in ApplicationCreateDTO

--- src/main/java/com/trackmatch/dto/application/ApplicationCreateDTO.java
+++ src/main/java/com/trackmatch/dto/application/ApplicationCreateDTO.java
@@ -1,11 +1,6 @@
 package com.trackmatch.dto.application;
 
-import com.trackmatch.domain.entities.EventEntity;
-import com.trackmatch.domain.entities.UserEntity;
 import com.trackmatch.domain.enums.ApplicationStatus;
-import com.trackmatch.dto.event.EventResponseDTO;
-import com.trackmatch.dto.user.UserDTO;
-import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDateTime;
