fd19831d063bff436e835322ccd4906734f372cf
Joilson Miranda <joilsonmirandasl@hotmail.com>
2025-06-15 00:20:34 -0300

    feat: add handleEnumError in GlobalExceptionHandler

--- src/main/java/com/trackmatch/exception/GlobalExceptionHandler.java
+++ src/main/java/com/trackmatch/exception/GlobalExceptionHandler.java
@@ -1,17 +1,20 @@
 package com.trackmatch.exception;
 
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
@@ -34,6 +37,38 @@ public class GlobalExceptionHandler {
 
         errors.put(ex.getType().name().toLowerCase(), ex.getMessage());
 
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse(400, errors));
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse(404, errors));
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> handleEnumError(HttpMessageNotReadableException ex) {
+        Throwable cause = ex.getCause();
+        if (cause instanceof InvalidFormatException formatEx && formatEx.getTargetType().isEnum()) {
+
+            String fieldName = "enumField";
+
+            if (!formatEx.getPath().isEmpty()) {
+                fieldName = formatEx.getPath().get(0).getFieldName();
+            }
+
+            String invalidValue = formatEx.getValue().toString();
+            String enumName = formatEx.getTargetType().getSimpleName();
+            String[] expectedValues = Arrays.stream(formatEx.getTargetType().getEnumConstants())
+                    .map(Object::toString)
+                    .toArray(String[]::new);
+
+            String message = String.format(
+                    "Valor inválido '%s' para o tipo %s. Valores esperados: %s",
+                    invalidValue,
+                    enumName,
+                    String.join(", ", expectedValues)
+            );
+
+            Map<String, String> errors = Map.of(fieldName, message);
+            return ResponseEntity.badRequest().body(new ErrorResponse(400, errors));
+        }
+
+        Map<String, String> errors = Map.of("error", "Requisição malformada");
+        return ResponseEntity.badRequest().body(new ErrorResponse(400, errors));
     }
 }
