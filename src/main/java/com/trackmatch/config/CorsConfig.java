8d9ba9b3c2d46271096c41d714168e1e8fbd0ff5
Joilson Miranda <joilsonmirandasl@hotmail.com>
2025-06-16 21:13:04 -0300

    fix: add swagger extra config in application.properties

--- src/main/java/com/trackmatch/config/CorsConfig.java
+++ src/main/java/com/trackmatch/config/CorsConfig.java
@@ -7,20 +7,26 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
-@Configuration()
+@Configuration
 public class CorsConfig implements WebMvcConfigurer {
+
     private final List<String> allowedOrigins;
 
-    public CorsConfig(@Value("#{'${cors.allowed-origins}'.split(',')}") List<String> allowedOrigins) {
+    public CorsConfig(
+            @Value("#{'${cors.allowed-origins}'.split('\\s*,\\s*')}")  // remove espaços
+            List<String> allowedOrigins) {
+
         this.allowedOrigins = allowedOrigins;
     }
-
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(allowedOrigins.toArray(String[]::new))
                 .allowedMethods("GET","POST","PUT","PATCH","DELETE","OPTIONS")
-                .allowCredentials(true);
+                .allowedHeaders("*")            // evita erro quando front envia Authorization, etc.
+                .exposedHeaders("Location")     // opcional: headers que o front pode ler
+                .allowCredentials(true)
+                .maxAge(3600);                  // cache do pre-flight
     }
 }
